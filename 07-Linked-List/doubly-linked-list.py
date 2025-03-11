cb7e32548324d99663edf83db66d3f51f7fe5c2c
Its-utsav <utsavdhimmar987@gmail.com>
2025-03-11 16:16:59 +0530

    implement basic operation of DLL

--- 07-Linked-List/doubly-linked-list.py
+++ 07-Linked-List/doubly-linked-list.py
@@ -11,19 +11,71 @@ class DoublyLinkedList:
         self.__size: int = 0
 
     def insert_at_start(self, value: int) -> Node:
-        pass
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+        self.__size += 1
+        return self.head
 
     def insert_at_end(self, value: int) -> Node:
-        pass
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            temp = self.head
+
+            while temp.next != None:
+                temp = temp.next
+
+            temp.next = new_node
+            new_node.prev = temp
+        self.__size += 1
+        return self.head
 
     def remove_from_start(self) -> Node:
-        pass
+        if not self.head:
+            raise Exception("Empty Linked List")
+
+        temp = self.head
+        self.head = temp.next
+        temp.next.prev = self.head
 
     def remove_from_end(self) -> Node:
-        pass
+        if not self.head:
+            raise Exception("Empty Linked List")
+
+        temp = self.head
+
+        while temp.next.next != None:
+            temp = temp.next
+
+        temp.next = None
 
     def __str__(self):
-        pass
+        s = "Null"
+        temp = self.head
+        while temp:
+            s += "->"
+
+            if temp.next == None:
+                s += f"{temp.data} ."
+            else:
+                s += f"{temp.data}"
+                s += "<-"
+
+            temp = temp.next
+        return s
 
 
 dll = DoublyLinkedList()
+dll.insert_at_start(10)
+dll.insert_at_start(100)
+dll.insert_at_end(20)
+dll.insert_at_end(200)
+dll.remove_from_end()
+print(dll)
